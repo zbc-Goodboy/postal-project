375725c68c47322caa5c7b436470d5e23c3cbb7d
zbc <1151624001@qq.com>
2020-10-29 23:42:13 +0800

    添加

--- src/main/java/com/example/postalproject/PostalProjectApplication.java
+++ src/main/java/com/example/postalproject/PostalProjectApplication.java
@@ -1,9 +1,11 @@
 package com.example.postalproject;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.postalproject.mapper")
 public class PostalProjectApplication {
 
     public static void main(String[] args) {
